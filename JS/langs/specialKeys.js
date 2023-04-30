9cfa5e6b0f984f2cf7c280cb8308369b5fb929cd
Nana Pisanko <bonjozzy@gmail.com>
2023-04-30 23:02:34 +0300

    docs: add lang letters, special symbols and keyboard btns

---
+++ JS/langs/specialKeys.js
@@ -0,0 +1,20 @@
+export const symbols = {
+  backquote: '`',
+  minus: '-',
+  equal: '=',
+  bracketleft: '[',
+  bracketright: ']',
+  backslash: '\\',
+  semicolon: ';',
+  quote: "'",
+  comma: ',',
+  period: '.',
+  slash: '/',
+  space: ' ',
+  tab: '  ',
+};
+
+export const specials = ['backspace', 'del', 'capslock', 'enter', 'shift', 'control', 'meta', 'alt', 'arrowup', 'arrowdown', 'arrowleft', 'arrowright'];
+
+export const keys = Object.keys(symbols);
+export const values = Object.values(symbols);
