e58fd81afec648796673022f7c45d5ff95deb6f7
Nana Pisanko <bonjozzy@gmail.com>
2023-08-03 17:09:41 +0300

    docs: add eslint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import state from './JS/modules/state/state.js';
+import state from "./JS/modules/state/state.js";
 import {
   createBlock,
-} from './JS/modules/create.js';
-import langLetters from './JS/langs/langLetters.js';
+} from "./JS/modules/create.js";
+import langLetters from "./JS/langs/langLetters.js";
 
-state.langFromLocalStorage = localStorage.getItem('langFromLocalStorage') || 'ru';
+state.langFromLocalStorage = localStorage.getItem("langFromLocalStorage") || "ru";
 createBlock(langLetters[state.langFromLocalStorage], state.isCapsOn);
